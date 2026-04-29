b08f0121237d3d81c22e58686887bd39de55aab2
johannes .D <j.duschl@tu-berlin.de>
2026-04-29 22:02:03 +0200

    getAllLegalMoves inprogress

--- src/main/java/app/Zuggenerator.java
+++ src/main/java/app/Zuggenerator.java
@@ -1,9 +1,31 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zuggenerator {
 
-    public String[] getAllLegalMoves(char[][] board){
-        return null;
+    public List<String> getAllLegalMoves(char[][] board){
+
+        List<String> possibleMoves = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+
+                char currentFigure = board[i][j];
+                switch (currentFigure){
+                    case '-' -> {
+                        break;
+                    }
+                    case 'w' -> {
+
+                    }
+                }
+
+            }
+        }
     }
+
+    private void addMovesForWhiteFigure(char[][] board, int i, int j, boolean isWhite)
 
 }
